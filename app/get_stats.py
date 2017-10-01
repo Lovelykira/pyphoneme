6c8bd55731bee11011bf49f3341edb8e0055a2a3
kostyachum <kostyachum@gmail.com>
2017-10-01 18:25:13 +0300

    to python3

--- app/get_stats.py
+++ app/get_stats.py
@@ -8,7 +8,7 @@ def main():
 	tokenizer = RegexpTokenizer(r'\w+')
 
 	data=[]
-	with open('../data/string.txt', 'r') as myfile:
+	with open('file.txt', 'r') as myfile:
 	    content=myfile.read().replace('\n', ' ')
 	myfile.close()
 
@@ -16,11 +16,11 @@ def main():
 
 	chunk_size=10
 	data = tokenizer.tokenize(content)
-	for iWord in xrange(0,chunk_size):
+	for iWord in range(0,chunk_size):
 		data.append('')
 
 	transcriptions=[]
-	for iWord in xrange(0,len(data),chunk_size):
+	for iWord in range(0,len(data),chunk_size):
 		se=' '.join(data[iWord:iWord+chunk_size])
 		tr=check_output(["espeak", "-q", "--ipa", '-v', 'en-us', se]).decode('utf-8')
 		print(se, ' ', tr)
